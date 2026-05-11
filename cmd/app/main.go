f97ff37b36f6a994ab98bdf3094db030aaf6cc5b
skrylov <krylovst.g@mail.ru>
2026-05-11 21:20:17 +0300

    added config.go

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"dungeon/internal/config"
+	"fmt"
+)
 
+func main() {
+	cfg := config.MustLoad()
+	err := cfg.Validate()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(cfg)
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,11 +1,72 @@
 package config
 
-import "time"
+import (
+	"flag"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
 
 type Config struct {
-	Floors           int `json:"floors"`
-	MonstersPerFloor int `json:"monsters_per_floor"`
+	Floors   int `json:"Floors"`
+	Monsters int `json:"Monsters"`
 
-	OpenAt   time.Time     `json:"open_at"`
-	Duration time.Duration `json:"duration"`
+	OpenAt   string `json:"OpenAt"`
+	Duration int    `json:"Duration"`
+}
+
+func MustLoad() *Config {
+	path := fetchConfigPath()
+	if path == "" {
+		panic("config path is empty")
+	}
+
+	return MustLoadByPath(path)
+}
+
+func MustLoadByPath(configPath string) *Config {
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		panic("config file doens't exist: " + configPath)
+	}
+	var cfg Config
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		panic("failed to read config: " + err.Error())
+	}
+
+	return &cfg
+}
+
+func fetchConfigPath() string {
+	var res string
+
+	flag.StringVar(&res, "config", "", "path to config file")
+	flag.Parse()
+
+	if res == "" {
+		res = os.Getenv("CONFIG_PATH")
+	}
+
+	return res
+}
+
+func (c *Config) Validate() error {
+	if c.Floors <= 0 {
+		return fmt.Errorf("floors must be positive")
+	}
+
+	if c.Monsters <= 0 {
+		return fmt.Errorf("monsters must be positive")
+	}
+
+	if _, err := time.Parse("15:04:05", c.OpenAt); err != nil {
+		return fmt.Errorf("invalid OpenAt format")
+	}
+
+	if c.Duration <= 0 {
+		return fmt.Errorf("duration must be positive")
+	}
+
+	return nil
 }
